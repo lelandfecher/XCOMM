8c4484fcc0070cc89d29a6fddab440631ef8d5b8
James Hollowell <jamesthollowell@gmail.com>
2016-03-29 17:05:25 -0400

    Work on JSON serialization

--- src/main/java/model/ClassStore.java
+++ src/main/java/model/ClassStore.java
@@ -1,5 +1,9 @@
 package model;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,7 +11,7 @@ import java.util.List;
  * Created by James Hollowell on 3/8/2016.
  */
 public class ClassStore {
-    private static ClassStore instance;
+    private static transient ClassStore instance;
     private List<Class> classes;
 
     private ClassStore() {
@@ -21,5 +25,30 @@ public class ClassStore {
         return instance;
     }
 
+    public void Load() throws FileNotFoundException {
+        Load("classes.json");
+    }
+
+    public void Load(String filePath) throws FileNotFoundException {
+        Gson gson = new Gson();
+        InputStreamReader isr = new InputStreamReader(new FileInputStream(filePath));
+        classes = (List<Class>) gson.fromJson(isr, classes.getClass());
+    }
+
+    public void Save() throws IOException {
+        Save("classes.json");
+    }
+
+    public void Save(String filePath) throws IOException {
+        File file = new File(filePath);
+        if (!file.exists() && !file.createNewFile()) {
+            throw new IOException("Could not create settings file");
+        }
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(this, classes.getClass());
+        FileWriter fileWriter = new FileWriter(file);
+        fileWriter.write(json);
+        fileWriter.close();
+    }
 
 }
