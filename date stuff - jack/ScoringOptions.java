1df918a48d348631f3c340464113985dfbe7fdca
Vociferix <jack.a.bernard.jr@gmail.com>
2016-03-29 16:39:22 -0400

    Potential Date Solution - Please Review
    I created a class called ClassDate that can be added in a Vector or List
    to the Student class.  Essentially all it does is store a date and
    attendance status for that date.  I have included an edited version of
    the Student class to use ClassDate, but I have not attempted to work
    this in with serialization, especially since it sounds like we will be
    going another route soon anyway.  Additionally, this allowed me to add a
    getScore method to Student which calculates the grade that student has
    for attendance based on the options found in my ScoringOptions class.

--- date stuff - jack/ScoringOptions.java
+++ date stuff - jack/ScoringOptions.java
@@ -1,20 +1,15 @@
-enum Method
-{
-	CountUp,
-	CountDown,
-	Average
-}
+// This class allows different options for how attendance is graded.
 
 public class ScoringOptions
 {
-	private double m_max;
-	private double m_presentPoints;
-	private double m_absentPoints;
-	private double m_tardyPoints;
-	private double m_tardyRatio;
-	private int m_graceAbsence;
-	private int m_graceTardy;
-	private Method m_method;
+	private double m_max; // The maximum score to start from - used for the CountDown method
+	private double m_presentPoints; // The number of points each date of present is worth - used for CountUp method
+	private double m_absentPoints; // The number of points for each date of absent is worth - used for CountDown method
+	private double m_tardyPoints; // The number of points for each date of tardy is worth - used for CountDown method
+	private double m_tardyRatio; // The percentage of a present that a tardy is worth as a value between 0 and 1 - used for Average method
+	private int m_graceAbsence; // The number of absences that will not be factored into the score (ex. Points will be deducted for each absence after 2)
+	private int m_graceTardy; // The number of tardies that will not be factored into the score
+	private Method m_method; // The method use for determining the score - see enumerator Method
 	
 	public double getMax()
 	{
@@ -95,4 +90,11 @@ public class ScoringOptions
 	{
 		m_method = method;
 	}
+}
+
+enum Method
+{
+	CountUp, // Start from 0 and count up for each present and (if wanted) tardy
+	CountDown, // Start from a max score and count down for each absent and tardy
+	Average // Calculate the number of days present and tardy out of all the days on record and score as a percent (out of 100)
 }
